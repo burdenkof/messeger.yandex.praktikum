9ce107fb6561ffcc7424aa4466901858583099dd
vboxuser <vboxuser@linux.myguest.virtualbox.org>
2023-03-10 03:43:55 +0500

    init commit

---
+++ components/chatrow/chatrow.js
@@ -0,0 +1,7 @@
+const { template } = require("handlebars");
+
+const chatRow = (item) =>{
+
+    const Handlebars = require("handlebars");
+    const template = Handlebars.compile(template);
+}
---
+++ components/chatrow/template.js
@@ -0,0 +1,4 @@
+export default chatRowTemplate =
+'<li>
+    <h1>{{item.name}}</h1>
+</li>'
