a420dc154e8c17418d200341b7fdc6edbaada71d
vboxuser <vboxuser@linux.myguest.virtualbox.org>
2023-03-10 05:10:21 +0500

    chat row component created

--- components/chatrow/chatrow.js
+++ components/chatrow/chatrow.js
@@ -1,12 +1,8 @@
 import {chatRowTemplate} from './template'
-const { template } = require("handlebars");
 
-const chatRow = (item) =>{
+ export const chatRow = (item) => {
 
     const Handlebars = require("handlebars")
     const template = Handlebars.compile(chatRowTemplate)
     return template(item)
-
-    
 }
-export default chatRow
