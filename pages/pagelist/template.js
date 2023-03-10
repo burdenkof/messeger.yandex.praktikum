0129db11cf0e81c9931d36a66ba2fc3be62894e0
vboxuser <vboxuser@linux.myguest.virtualbox.org>
2023-03-10 07:12:39 +0500

    chat row component done

---
+++ pages/pagelist/template.js
@@ -0,0 +1,8 @@
+export const  pagelistTemplate =
+`
+
+<div class="pages-pagelist">
+    <a href="#chatlist">Chat list ></a>
+</div>
+
+`
---
+++ src/route.js
@@ -0,0 +1,20 @@
+import { chatRow } from "../components/chatrow/chatrow";
+import { pagelist } from "../pages/pagelist/pagelist";
+
+export const getPage = (url) => {
+    switch (url) {
+      case "#chatlist":
+        return chatRow({item:
+            {
+                name:'Chat Name', 
+                userName:'Will smith', 
+                text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+                time: '09.09.2023 19:45'
+            }
+            
+        })
+      default:
+        return pagelist()
+    }
+}
+  
