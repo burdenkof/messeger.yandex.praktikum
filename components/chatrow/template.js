c2818bfcaf91988e825d9b93c68eb587af7feca8
vboxuser <vboxuser@linux.myguest.virtualbox.org>
2023-03-10 07:11:55 +0500

    chat row component done

--- components/chatrow/template.js
+++ components/chatrow/template.js
@@ -1,4 +1,18 @@
 export const  chatRowTemplate =
-`<li>
-    <h1>{{item.name}}</h1>
-</li>`
+`<div class="chat-list_one-row">
+    <div class="chat-list_one-row_avatar" >
+        
+    </div>
+    <div class="chat-list_one-row_main-info"  >
+    <div class="chat-list_one-row_main-info_time">{{item.time}}</div>
+
+        <div class="chat-list_one-row_main-info_chat-name">{{item.name}}</div>
+        
+        <div class="chat-list_one-row_main-info_description">
+            <span class="chat-list_one-row_main-info_description_username">{{item.userName}}:&nbsp;</span>
+            <span class="chat-list_one-row_main-info_description_text">{{item.text}}</span>
+            
+        </div>
+        
+    </div>
+</div>`
